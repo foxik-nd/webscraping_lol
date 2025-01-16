16793800cd361db5706e90e9f20eb79edf9d4379
foxik-nd <froxik77430@outlook.fr>
2025-01-16 12:17:34 +0100

    get name image + readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# webscraping_lol
+# Projet de récupération des champions League of Legends
+
+## Équipe
+- **Nassim Y**
+- **Nicolas D**
+
+## Justification du choix de Scrapy
+Scrapy est un framework Python spécialement conçu pour le web scraping. Il offre une structure de projet claire, une gestion asynchrone des requêtes et une communauté active. Cela le rend particulièrement efficace pour extraire des listes structurées (comme celles des champions) à partir de pages web.
+
+## Étapes pour lancer le projet
+
+1. **Lancer la commande Scrapy** :  
+   ```
+   scrapy crawl championspider -o champions.json
--- scraplol/scraplol/spiders/championspider.py
+++ scraplol/scraplol/spiders/championspider.py
@@ -1,10 +1,22 @@
 import scrapy
 
-
 class ChampionspiderSpider(scrapy.Spider):
     name = "championspider"
     allowed_domains = ["leagueoflegends.fandom.com"]
-    start_urls = ["https://leagueoflegends.fandom.com/wiki/League_of_Legends_Wiki"]
+    start_urls = ["https://leagueoflegends.fandom.com/wiki/List_of_champions"]
 
     def parse(self, response):
-        pass
+        champions = response.css(".champion-icon a::attr(href)").getall()
+        for link in champions:
+            yield response.follow(link, self.parse_champion)
+
+    def parse_champion(self, response):
+        yield {
+            'name': response.css("h1.page-header__title::text").get().split(" (")[0].strip(),
+            'image_url': response.css(".pi-image-thumbnail::attr(src)").get(),
+            # 'position': response.css(".pi-item[data-source='position'] .pi-data-value::text").get(),
+            # 'role': response.css(".pi-item[data-source='class'] .pi-data-value::text").get(),
+            # 'damage_type': response.css(".pi-item[data-source='damage'] .pi-data-value::text").get(),
+            # 'resource': response.css(".pi-item[data-source='resource'] .pi-data-value::text").get(),
+            # 'health':response.css(".pi-smart-data-value pi-data-value pi-font pi-item-spacing pi-border-color::text").get()
+        }
